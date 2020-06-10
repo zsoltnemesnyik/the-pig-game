6fe4dbf9c8fbe7e9c1ff33db7c149ffba55c8b51
NYKFAN <nemesnyik.zsolt@gmail.com>
2020-06-10 12:47:26 +0200

    activate toggle-icon and rules

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import '../scss/main.scss';
+import {elements} from './views/base';
 
 // import images to ./dist
 function importAll(r) {
@@ -7,3 +8,10 @@ function importAll(r) {
 }
 
 const images = importAll(require.context('../img/', true, /\.(png|jpe?g|svg)$/));
+
+// Toggle classes
+elements.rulesIcon.addEventListener('click', () => {
+    elements.rulesIcon.classList.toggle('open');
+    elements.rules.classList.toggle('open');
+    elements.bodyNotRules.style.filter = elements.bodyNotRules.style.filter == 'blur(5px) brightness(30%)' ? '' : 'blur(5px) brightness(30%)';
+});
---
+++ src/js/views/base.js
@@ -0,0 +1,5 @@
+export const elements = {
+    bodyNotRules: document.querySelector('body div:not(.rules)'),
+    rules: document.querySelector('.rules'),
+    rulesIcon: document.querySelector('.toggle-icon')
+}
